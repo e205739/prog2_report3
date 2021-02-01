ab89a2745b3181995fc3c58680e85bb09d7057ec
城間香凜 <karin@shiromakarinnoMacBook-Air.local>
2021-02-01 15:24:41 +0900

    report4 ステップ1終了。

--- app/src/main/java/jp/ac/uryukyu/ie/e205739/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205739/Enemy.java
@@ -1,73 +1,20 @@
 package jp.ac.uryukyu.ie.e205739;
 
-/**
- * 敵クラス。(これらをカプセル化した)
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- *  
- */
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+public class Enemy extends LivingThing  {
 
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
-     */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
+        super(name, maximumHP, attack);
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        if( dead == true ) {
-            damage = 0;
-        }
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
-    }
-
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
-    }
-    /**
-     * カプセル化されたNameをアクセスできるようにするための関数を用意した
-     * @return
-     */
-    public String getName(){
-        return this.name;
+
     }
 
-    /**
-     * カプセル化されたDeadをアクセスできるようにするための関数を用意した。
-     * インスタンス化したEnemyに対して使う。
-     * @return
-     */
-    public Boolean getDead(){
-        return this.dead;
-    }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e205739/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205739/Hero.java
@@ -1,79 +1,20 @@
 package jp.ac.uryukyu.ie.e205739;
 
-/**
- * ヒーロークラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
- */
-public class Hero {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+public class Hero extends LivingThing  {
 
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name ヒーロー名
-     * @param maximumHP ヒーローのHP
-     * @param attack ヒーローの攻撃力
-     */
     public Hero (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
+        super(name, maximumHP, attack);
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
-    /**
-     * Enemyへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param e 攻撃対象
-     */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
-    }
-
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
             dead = true;
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
         }
-    }
-    /**
-     * カプセル化されたNameをアクセスできるようにするための関数を用意した。
-     * @return
-     */
-    public String getName(){
-        return this.name;
-    }
-    
-    /**
-     * カプセル化されたDeadをアクセスできるようにするための関数を用意した。
-     * インスタンス化したHeroに対して使う。
-     * @return Dead boolean
-     */
-    public boolean getDead(){
-        return this.dead;
+
     }
 
-    /**
-     * カプセル化されたhitPointをアクセスできるようにするための関数を用意した。
-     * インスタンス化したHeroに対して使う。
-     * @return hitPoint int
-     */
-    public int gethitPoint(){
-        return this.hitPoint;
-    }
 }
