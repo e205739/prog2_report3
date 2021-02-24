50044b292223123bf46fd3c0d50e99059091c259
城間香凜 <karin@shiromakarinnoMacBook-Air.local>
2021-02-24 14:25:16 +0900

    report2 LivingThing.javaの追加

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205739/LivingThing.java
@@ -0,0 +1,68 @@
+package jp.ac.uryukyu.ie.e205739;
+
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name 名前
+     * @param maximumHP HP
+     * @param attack 攻撃力
+     */
+    public LivingThing (String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+    /**
+     * カプセル化されたDeadをアクセスできるようにするための関数を用意した。
+     * インスタンス化したHeroに対して使う。
+     * @return Dead boolean
+     */
+    public boolean isDead(){
+        return this.dead;
+    }
+
+    /**
+     * カプセル化されたNameをアクセスできるようにするための関数を用意した。
+     * @return
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * Enemyへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param e 攻撃対象
+     */
+    public void attack(LivingThing opponent){
+        int damage = (int)(Math.random() * attack);
+        if( dead == true ) {
+            damage = 0;
+        }
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+        opponent.wounded(damage);
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        }
+    }
+    
+    
+}
